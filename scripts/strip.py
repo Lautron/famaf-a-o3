d4e3d791f72b03c41c3d0c8c6d5e5c4eeae32e66
Lautaro Bachmann <laurigbach@gmail.com>
2023-03-27 11:06:40 -0300

    Añadir script para extraer todas la paginas de un pdf que esten resaltadas

---
+++ scripts/strip.py
@@ -0,0 +1,28 @@
+import fitz  # pip install pymupdf
+import sys
+
+
+def has_highlight(page):
+    """Check if a page has any highlight annotations."""
+    for annot in page.annots():
+        if annot.type[1] == "Highlight":
+            return True
+    return False
+
+
+def main(filename):
+    doc = fitz.open(filename)
+    new_doc = fitz.open()
+
+    for page in doc:
+        if has_highlight(page):
+            print(f"Page {page.number} has highlight")
+            new_doc.insert_pdf(doc, from_page=page.number, to_page=page.number)
+
+    new_doc.save("Caps (PONER CAPS DEL PDF)_" + filename)
+
+
+if __name__ == "__main__":
+    filenames = sys.argv[1:]
+    for filename in filenames:
+        main(filename)
