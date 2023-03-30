4031f3b013198d1540dca92fdc7a1d7065b50197
Lautron <66799530+Lautron@users.noreply.github.com>
2023-03-30 10:58:27 -0300

    Update pandoc.sh
    Hacer que la TOC tenga una profundidad de 4 y cambiar el tipo de documento a scrarticl

--- scripts/pandoc.sh
+++ scripts/pandoc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for file in "$@"; do
-  pandoc --pdf-engine=xelatex --toc -f markdown -t latex "$file" -o "${file%.md}.pdf"
+  pandoc --pdf-engine=xelatex --toc --toc-depth=4 -M documentclass=scrartcl  -f markdown -t latex "$file" -o "${file%.md}.pdf"
 done
